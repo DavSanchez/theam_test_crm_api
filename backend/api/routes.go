ed3f64a69298f659283c822941cffb626830e198
David Sánchez <d.vinternatt@gmail.com>
2020-03-14 03:50:42 +0000

    Removed test main route

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -9,10 +9,6 @@ import (
 
 var Router = mux.NewRouter()
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
-}
-
 func listAllCustomers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("List all customers"))
 }
@@ -43,8 +39,6 @@ func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 }
 
 func InitRouter() {
-	Router.HandleFunc("/", home) // API root, testing
-
 	customers := Router.PathPrefix("/customers").Subrouter() // Customer subroute for the API
 
 	customers.HandleFunc("/list", listAllCustomers).Methods("GET")
