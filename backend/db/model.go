d53cd70e095ca4b88bd7c2a50eaf684fca5dbf1d
David Sánchez <d.vinternatt@gmail.com>
2020-03-15 03:31:58 +0000

    Implemented database queries

--- backend/db/model.go
+++ backend/db/model.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"golang.org/x/crypto/bcrypt"
@@ -41,36 +40,81 @@ func (ns *NullString) MarshalJSON() ([]byte, error) {
 
 // Functions for interacting with DB
 
-func (c *Customer) ListAllCustomers(db *sql.DB) error {
-	return errors.New("Error")
-}
-
 func (c *Customer) GetCustomer(db *sql.DB) error {
-	return errors.New("Error")
+	return db.QueryRow(`
+		SELECT (name, surname, photoUrl, lastModifiedByUserId)
+		FROM customers
+		WHERE id = $1
+		`, c.Id).Scan(&c.Name, &c.Surname, &c.PhotoURL, &c.LastModifiedByUserId)
 }
 
 func (c *Customer) CreateCustomer(db *sql.DB) error {
-	return errors.New("Error")
+	err := db.QueryRow(`
+		INSERT INTO customers (name, surname, photoUrl, lastModifiedByUserId)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id
+		`, c.Name, c.Surname, c.PhotoURL, c.LastModifiedByUserId).Scan(&c.Id)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (c *Customer) UpdateCustomer(db *sql.DB) error {
-	return errors.New("Error")
+	_, err := db.Exec(`
+		UPDATE customers
+		SET (name=$1, surname=$2, photoUrl=$3, lastModifiedByUserId=$4)
+		WHERE id=$4
+		`, c.Name, c.Surname, c.PhotoURL, c.LastModifiedByUserId, c.Id)
+
+	return err
 }
 
 func (c *Customer) DeleteCustomer(db *sql.DB) error {
-	return errors.New("Error")
+	_, err := db.Exec(`
+		DELETE FROM customers
+		WHERE id=$1
+		`, c.Id)
+
+	return err
 }
 
-func InsertUserIfNotExists(db *sql.DB, username, password string) error {
-	passwdHash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+func ListAllCustomers(db *sql.DB) ([]Customer, error) {
+	rows, err := db.Query(`
+		SELECT (id, name, surname, photoUrl, lastModifiedByUserId)
+		FROM customers`)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	customers := []Customer{}
+
+	for rows.Next() {
+		var c Customer
+		err := rows.Scan(&c.Id, &c.Name, &c.Surname, &c.PhotoURL, &c.LastModifiedByUserId)
+		if err != nil {
+			return nil, err
+		}
+		customers = append(customers, c)
+	}
+
+	return customers, nil
+}
+
+func (u *User) InsertUserIfNotExists(db *sql.DB) error {
+	passwdHash, err := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
 	CheckErr(err)
 
 	_, err = db.Exec(`
 		INSERT INTO users(username, password)
-		VALUES ($1, $2) ON CONFLICT DO NOTHING`, username, string(passwdHash))
+		VALUES ($1, $2) ON CONFLICT DO NOTHING`, u.Username, string(passwdHash))
+
 	CheckErr(err)
 
-	fmt.Println("Inserted a new user")
+	fmt.Println("Inserted initial user if it didn't exist ")
 
 	return err
 }
