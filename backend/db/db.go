cf13fe3c4b266dee69e0e9998ff3258c64a2c04f
David Sánchez <d.vinternatt@gmail.com>
2020-03-15 03:32:20 +0000

    Refactor

--- backend/db/db.go
+++ backend/db/db.go
@@ -5,52 +5,61 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	_ "github.com/lib/pq"
 )
 
-var ( /* constants */
+var ( /* constants and database global object */
 	db_name = os.Getenv("DB_NAME")
 	db_user = os.Getenv("DB_USER")
 	db_pass = os.Getenv("DB_PASS")
 	db_host = os.Getenv("DB_HOST")
+
+	DB *sql.DB
 )
 
-// TODO: Need a Database object for the routes to access it!!
-
 func InitDB() {
-	time.Sleep(5 * time.Second) // Wait for database to set up (Docker, just in case!)
-	db_params_str := fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=disable", db_name, db_user, db_pass, db_host)
-	db, err := sql.Open("postgres", db_params_str)
+	var err error
+
+	db_params_str := fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=disable",
+		db_name,
+		db_user,
+		db_pass,
+		db_host)
+
+	DB, err = sql.Open("postgres", db_params_str)
 
 	CheckErr(err)
-	defer db.Close()
 
-	err = db.Ping()
+	err = DB.Ping()
 
 	CheckErr(err)
 	log.Print("Connected to database")
 
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users(
+	_, err = DB.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
 			id SERIAL PRIMARY KEY,
 			username VARCHAR(32) UNIQUE NOT NULL,
 			password VARCHAR(64) NOT NULL
 		)`)
 	CheckErr(err)
 
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS customers(
+	_, err = DB.Exec(`
+		CREATE TABLE IF NOT EXISTS customers (
 			id SERIAL PRIMARY KEY,
 			name VARCHAR(32) NOT NULL,
 			surname VARCHAR(32) NOT NULL,
-			photo_url TEXT,
-			last_modified_by_user_id INT REFERENCES users(id)
+			photoUrl TEXT,
+			lastModifiedByUserId INT REFERENCES users(id)
 		)`)
 	CheckErr(err)
 
-	err = InsertUserIfNotExists(db, "Admin", "secret123")
+	initialUser := User{
+		Username: "Admin",
+		Password: "Secret123",
+	}
+
+	err = initialUser.InsertUserIfNotExists(DB)
 	CheckErr(err)
 
 }
