b148de7d0d515fd92edd2bb5906795e173928f3a
David Sánchez <d.vinternatt@gmail.com>
2020-03-16 15:04:40 +0000

    Refactor utils, add random filename generator.
    new function inspired by Go's own ioutil.TempFile()

--- utils/utils.go
+++ utils/utils.go
@@ -4,11 +4,18 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"strconv"
 )
 
-const pathToImagesDir = "img/"
+const (
+	pathToImagesDir = "img"
+	PathFileServer  = "static"
+)
 
 func FileUpload(r *http.Request) (string, error) {
 	r.ParseMultipartForm(32 << 20) // Max memory 32 MiB
@@ -19,16 +26,16 @@ func FileUpload(r *http.Request) (string, error) {
 	}
 	defer file.Close() // Close the file when finished
 
-	fullPath := pathToImagesDir + handler.Filename
-	f, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, 0666)
+	newFileName, err := generateRandomFilename(pathToImagesDir, handler.Filename)
 	if err != nil {
-		log.Printf("Error opening file: %s", err.Error())
+		log.Printf("Error creating file: %s", err.Error())
 		return "", err
 	}
+	f, _ := os.OpenFile(path.Join(pathToImagesDir, newFileName), os.O_WRONLY|os.O_CREATE, 0666)
 	defer f.Close()
 
 	io.Copy(f, file)
-	return fullPath, nil
+	return path.Join(PathFileServer, newFileName), nil
 }
 
 func ResponseJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -43,4 +50,18 @@ func CheckErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func generateRandomFilename(dir, filename string) (name string, err error) {
+	for i := 0; i < 10000; i++ {
+		name = strconv.Itoa(rand.Int()) + path.Ext(filename)
+		namePath := filepath.Join(dir, name)
+		_, err := os.OpenFile(namePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+		defer os.Remove(namePath)
+		if os.IsExist(err) {
+			continue
+		}
+		break
+	}
+	return
 }
